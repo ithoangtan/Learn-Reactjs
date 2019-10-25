e1cbed779cbafa6bb498f9cae987b05dc97de66a
Hoàng Tấn <ithoangtan@gmail.com>
2019-10-25 23:24:30 +0700

    constructor and this.todoItems = []; {this.todoItems.map((item, index) => (             <TodoItem key={index} title={item} />

--- CoderX/todo-list/src/App.js
+++ CoderX/todo-list/src/App.js
@@ -1,31 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import TodoItem from "./Components/TodoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem title="Đi chợ"/>
-        <TodoItem title="Học reactjs"/>
-        <TodoItem option={this.tenBien}/>
-        <TodoItem option={this.tenObject}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+    this.todoItems = ["go to market", "learn UI/UX"];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.todoItems.map((item, index) => (
+            <TodoItem key={index} title={item} />
+          ))}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- CoderX/todo-list/src/Components/TodoItem.js
+++ CoderX/todo-list/src/Components/TodoItem.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
 class TodoItem extends Component {
+
   render() {
     return (
       <div>
         <p> {this.props.title} </p>
-        <p> {this.props.option} </p>
       </div>
     );
   }
