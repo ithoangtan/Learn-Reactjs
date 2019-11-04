0836750188f642d2168f41a119516d90b9dbed39
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-04 23:43:59 +0700

    try mui-button

--- learning-redux-sage-material-ui-redux-form/src/App.js
+++ learning-redux-sage-material-ui-redux-form/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
       <p> Hello redux-saga</p>
+      <Button variant="contained" color="primary">
+        Material Button
+      </Button>
     </div>
   );
 }
