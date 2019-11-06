39dec23e27e9ed7b03838646d57cb0a522f0d568
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-06 23:51:44 +0700

    GUI task filtered with Card, CardContent, CardActions, Box, Typography

--- learning-redux-sage-material-ui-redux-form/src/containers/Taskboard/index.js
+++ learning-redux-sage-material-ui-redux-form/src/containers/Taskboard/index.js
@@ -2,22 +2,67 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 
-import Grid from "@material-ui/core/Grid";
-
-import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 
 import { STATUSES } from "../../constants/index";
+import {
+  Grid,
+  Box,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Typography
+} from "@material-ui/core";
+
+const listTask = [
+  { id: 1, title: "Read book", description: "Read material book", status: 0 },
+  { id: 2, title: "Read book1", description: "Read material book1", status: 2 },
+  { id: 3, title: "Read book2", description: "", status: 1 }
+];
 
 class TaskBoard extends Component {
   renderBoard() {
+    const { classes } = this.props;
     let xhtml = null;
     xhtml = (
       <Grid container spacing={2}>
+        {/* Filter các status từ const STATUSES theo staus.value */}
         {STATUSES.map((status, index) => {
+          const taskFiltered = listTask.filter(
+            task => task.status === status.value
+          );
           return (
             <Grid item md={4} xs={12} key={status.value}>
-              {status.label}
+              {/* Sử dụng Box để chúng ta margin và padding các phần tử */}
+              {/* Ngoài ra chúng ta cón thể css-in-js để tiến hành code xem lại ở commit css-in-js*/}
+              <Box mt={1} mb={1}>
+                <div className={classes.status}>{status.label}</div>
+              </Box>
+              <div className={classes.wrapperListTask}>
+                {/* task đã được filter sẽ đổ vào Card tương ứng */}
+                {taskFiltered.map(task => {
+                  const { title, description, status } = task;
+                  return (
+                    <Card key={task.id} className={classes.card}>
+                      <CardContent>
+                        <Grid container justify="space-between">
+                          <Grid item md={8}>
+                            {/* Typography với atrribute component sẽ bao bọc giá trị của nó với tag có tên tương ứng */}
+                            <Typography component="h2">{title}</Typography>
+                          </Grid>
+                          <Grid item md={4}>
+                            {status.label}
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small"></Button>
+                      </CardActions>
+                    </Card>
+                  );
+                })}
+              </div>
             </Grid>
           );
         })}
