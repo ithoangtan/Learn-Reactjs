725c7eb2558799c23e6039c25b34613fb734037e
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-06 23:23:39 +0700

    Grid layout and render Grid item

--- learning-redux-sage-material-ui-redux-form/src/containers/Taskboard/styles.js
+++ learning-redux-sage-material-ui-redux-form/src/containers/Taskboard/styles.js
@@ -1,21 +1,3 @@
-const styles = theme => ({
-  taskBoard: {
-    display: "flex",
-    alignItems: "center",
-    backgroundColor: theme.color.primary
-  },
-  shape: {
-    // backgroundColor: "red",
-    backgroundColor: theme.shape.backgroundColor,
-    // color: "white",
-    color: theme.shape.textColor,
-    // borderColor: "#cccccc",
-    borderColor: theme.shape.border,
-    padding: "20px",
-    margin: "10px",
-    // borderRadius: "3px"
-    borderRadius: theme.shape.borderRadius
-  }
-});
+const styles = theme => ({});
 
 export default styles;
