59f08e4f8614649a267e24d93608bcd1655073a6
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-07 15:28:46 +0700

    config eslint level 1

--- learning-redux-sage-material-ui-redux-form/.eslintrc.js
+++ learning-redux-sage-material-ui-redux-form/.eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
    parser: "babel-eslint",
+   // parserOptions: {
+   //    ecmaVersion: 6,
+   //    sourceType: "module",
+   //    ecmaFeatures: {
+   //       jsx: true
+   //    }
+   // },
+   // extends: "eslint:recommended",
    rules: {
       semi: 1,
       quotes: [2, "double"]
+      // "react/prop-types": 1
    }
 };
--- learning-redux-sage-material-ui-redux-form/src/containers/Taskboard/index.js
+++ learning-redux-sage-material-ui-redux-form/src/containers/Taskboard/index.js
@@ -11,74 +11,79 @@ import TaskList from "./../../components/TaskList/index";
 import TaskForm from "./../../components/TaskForm/index";
 
 const listTask = [
-  { id: 1, title: "Read book", description: "Read material book", status: 0 },
-  { id: 2, title: "Read book1", description: "Read material book1", status: 2 },
-  { id: 3, title: "Read book2", description: "", status: 1 }
+   { id: 1, title: "Read book", description: "Read material book", status: 0 },
+   {
+      id: 2,
+      title: "Read book1",
+      description: "Read material book1",
+      status: 2
+   },
+   { id: 3, title: "Read book2", description: "", status: 1 }
 ];
 
 class TaskBoard extends Component {
-  state = {
-    open: false
-  };
-
-  renderBoard() {
-    let xhtml = null;
-    xhtml = (
-      <Grid container spacing={2}>
-        {/* Filter các status từ const STATUSES theo staus.value */}
-        {STATUSES.map((status, index) => {
-          const taskFiltered = listTask.filter(
-            task => task.status === status.value
-          );
-          return (
-            <TaskList
-              key={index}
-              tasks={taskFiltered}
-              status={status}
-            ></TaskList>
-          );
-        })}
-      </Grid>
-    );
-    return xhtml;
-  }
-
-  handleCloseForm = () => {
-    this.setState({
+   state = {
       open: false
-    });
-  };
+   };
 
-  renderForm() {
-    let xhtml = null;
-    const { open } = this.state;
-    xhtml = <TaskForm open={open} onClose={this.handleCloseForm}></TaskForm>;
-    return xhtml;
-  }
+   renderBoard() {
+      let xhtml = null;
+      xhtml = (
+         <Grid container spacing={2}>
+            {/* Filter các status từ const STATUSES theo staus.value */}
+            {STATUSES.map((status, index) => {
+               const taskFiltered = listTask.filter(
+                  task => task.status === status.value
+               );
+               return (
+                  <TaskList
+                     key={index}
+                     tasks={taskFiltered}
+                     status={status}
+                  ></TaskList>
+               );
+            })}
+         </Grid>
+      );
+      return xhtml;
+   }
 
-  addNewTaskOpenForm = () => {
-    this.setState({
-      open: true
-    });
-  };
+   handleCloseForm = () => {
+      this.setState({
+         open: false
+      });
+   };
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.taskBoard}>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          onClick={this.addNewTaskOpenForm}
-        >
-          <AddIcon /> Thêm mới công việc
-        </Button>
-        {this.renderBoard()}
-        {this.renderForm()}
-      </div>
-    );
-  }
+   renderForm() {
+      let xhtml = null;
+      const { open } = this.state;
+      xhtml = <TaskForm open={open} onClose={this.handleCloseForm}></TaskForm>;
+      return xhtml;
+   }
+
+   addNewTaskOpenForm = () => {
+      this.setState({
+         open: true
+      });
+   };
+
+   render() {
+      const { classes } = this.props;
+      return (
+         <div className={classes.taskBoard}>
+            <Button
+               variant="contained"
+               color="primary"
+               className={classes.button}
+               onClick={this.addNewTaskOpenForm}
+            >
+               <AddIcon /> Thêm mới công việc
+            </Button>
+            {this.renderBoard()}
+            {this.renderForm()}
+         </div>
+      );
+   }
 }
 
 export default withStyles(styles)(TaskBoard);
