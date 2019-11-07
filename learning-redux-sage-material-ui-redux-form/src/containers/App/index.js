9a179be2e36423219adab4e653f43623bae3b2be
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-07 21:43:55 +0700

    redux, redux tool config: reducers, store, compose

--- learning-redux-sage-material-ui-redux-form/src/containers/App/index.js
+++ learning-redux-sage-material-ui-redux-form/src/containers/App/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import { Provider } from "react-redux";
+import configStore from "../../redux/configStore.redux";
+
 import { withStyles } from "@material-ui/core";
 
 import styles from "./styles";
@@ -7,12 +10,16 @@ import theme from "../../commons/theme";
 import { ThemeProvider } from "@material-ui/styles";
 import Taskboard from "../Taskboard";
 
+const store = configStore();
+
 class App extends Component {
    render() {
       return (
-         <ThemeProvider theme={theme}>
-            <Taskboard />
-         </ThemeProvider>
+         <Provider store={store}>
+            <ThemeProvider theme={theme}>
+               <Taskboard />
+            </ThemeProvider>
+         </Provider>
       );
    }
 }
